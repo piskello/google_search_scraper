a85d64d54f808ee1e1f627787c93a00ff471cf73
Matteo Moretti <hello@matteomoretti.com>
2020-04-22 22:29:34 +0200

    good but not working

--- search.py
+++ search.py
@@ -9,33 +9,36 @@ headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML
 
 #Definisco i dati pr la ricerca
 ricerca="'paura' covid coronavirus"
-startDate = date(2020, 4, 1)
-endDate = date(2020, 4, 5)
+startDate = date(2020, 4, 2)
+endDate = date(2020, 4, 2) #5520
 
 #coversione per la query di ricerca
 deltaDate=endDate-startDate
 
 print("--------------------\nQuery: " + ricerca)
-print("From: " + startDate.strftime("%m/%d/%Y")+" to: "+endDate.strftime("%m/%d/%Y")+"\n--------------------")
-
-
+print("From: " + startDate.strftime("%d/%m/%Y")+" to: "+endDate.strftime("%d/%m/%Y")+"\n--------------------")
+print(deltaDate.days)
+ 
 for i in range(deltaDate.days+1):
-    startDate+=timedelta(i)
+    
     #moduli query di ricerca
     qHeader="https://www.google.com/search?q="
     qText=ricerca.replace("'","%22").replace(" ","+")
     qTimestamp="&safe=active&sxsrf=ALeKk02GfGyt6J2O2C16qAgaIQHUaTO2tw%3A1587473771795"
     qSource="&source=lnt"
-    qTimeSpan="&tbs=cdr%3A1%2C"
-    qStartDate="%2Ccd_min%3A"+startDate.strftime("%m/%d/%Y").replace("/","%2F")
-    qEndDate="%2Ccd_max%3A"+ startDate.strftime("%m/%d/%Y").replace("/","%2F")
+    qTimeSpan="&tbs=cdr%3A1%2C%2C"
+    qStartDate="cd_min%3A"+ startDate.strftime("%d/%m/%Y").replace("/","%2F")+"%2C"
+    qEndDate="cd_max%3A"+ startDate.strftime("%d/%m/%Y").replace("/","%2F")
     qNews="&tbm=nws"
 
     #query finale
     URL=qHeader+qText+qTimestamp+qSource+qTimeSpan+qStartDate+qEndDate+qNews
+    #print(URL)
     result = requests.get(URL, headers=headers)    
     soup = BeautifulSoup(result.content, 'html.parser')
-    total_results_text = soup.find("div", {"id": "result-stats"}).find(text=True, recursive=False) # this will give you the outer text which is like 'About 1,410,000,000 results'
-    results_num = ''.join([num for num in total_results_text if num.isdigit()]) # now will clean it up and remove all the characters that are not a number .
-    print(startDate.strftime("%m/%d/%Y")+ " " +results_num)
+    total_results_text = soup.find("div", {"id": "result-stats"}).find(text=True, recursive=False)
+    results_num = ''.join([num for num in total_results_text if num.isdigit()])
+    print(startDate.strftime("%d/%m/%Y")+ " " +results_num)
+    
+    startDate+=timedelta(1)
 
